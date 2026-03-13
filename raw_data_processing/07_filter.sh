eef1f19c90ec6ee90a12e1202b6049343e16e0f5
plutoxliu <117046241+plutoxliu@users.noreply.github.com>
2026-03-14 00:24:20 +1300

    Update 07_filter.sh

--- raw_data_processing/07_filter.sh
+++ raw_data_processing/07_filter.sh
@@ -8,39 +8,20 @@
 cd /nesi/project/uoo03773/ALL3/AUClean
 module load VCFtools/0.1.15-GCC-9.2.0-Perl-5.30.1
 
-echo "start, 70%"
+###Only example here. SNP filtering is a bit arbitrary, there's a balance betweeen filtering out uninformative SNPs, filtering out low quality SNPs, and then also ensuring that you retain enough SNPs
+#--max-missing: allowed proportion of missing data
+#--maf: minor allele frequency
+#--minDP: minimum depth
+#--missing-indv: generates a file reporting the missingness on a per-individual basis
+#--minQ: can start from minimum quality of 30
+#--min-meanDP: minimum mean depth of SNPs
+echo "start"
 
 vcftools --vcf populations.snps.vcf --max-missing 0.7 --maf 0.05 --minDP 3 --recode --recode-INFO-all --out miss70maf5dp3
 vcftools --vcf miss70maf5dp3.recode.vcf --missing-indv
-awk '$5 > 0.80' out.imiss | cut -f1 > lowDP-80.indv
-vcftools --vcf miss70maf5dp3.recode.vcf --remove lowDP-80.indv --recode --recode-INFO-all --out miss70maf5dp3INDV
+awk '$5 > 0.80' out.imiss | cut -f1 > lowDP-80.indv #make a file of the individuals that are over 80% missing
+vcftools --vcf miss70maf5dp3.recode.vcf --remove lowDP-80.indv --recode --recode-INFO-all --out miss70maf5dp3INDV # filter out those individuals
 vcftools --vcf miss70maf5dp3INDV.recode.vcf --min-meanDP 5 --recode --recode-INFO-all --out final70
-vcftools --vcf final70.recode.vcf --missing-site
+vcftools --vcf final70.recode.vcf --missing-site #see which sites/SNPs have a lot of missingness
 
-echo "done70%, do 50%"
-
-vcftools --vcf populations.snps.vcf --max-missing 0.5 --maf 0.05 --minDP 3 --recode --recode-INFO-all --out miss50maf5dp3
-vcftools --vcf miss50maf5dp3.recode.vcf --missing-indv
-awk '$5 > 0.80' out.imiss | cut -f1 > lowDP-80.indv
-vcftools --vcf miss50maf5dp3.recode.vcf --remove lowDP-80.indv --recode --recode-INFO-all --out miss50maf5dp3INDV
-vcftools --vcf miss50maf5dp3INDV.recode.vcf --min-meanDP 5 --recode --recode-INFO-all --out final50
-vcftools --vcf final50.recode.vcf --missing-site
-
-echo "done50%, do 30%"
-
-vcftools --vcf populations.snps.vcf --max-missing 0.3 --maf 0.05 --minDP 3 --recode --recode-INFO-all --out miss30maf5dp3
-vcftools --vcf miss30maf5dp3.recode.vcf --missing-indv
-awk '$5 > 0.80' out.imiss | cut -f1 > lowDP-80.indv
-vcftools --vcf miss30maf5dp3.recode.vcf --remove lowDP-80.indv --recode --recode-INFO-all --out miss30maf5dp3INDV
-vcftools --vcf miss30maf5dp3INDV.recode.vcf --min-meanDP 5 --recode --recode-INFO-all --out final30
-vcftools --vcf final30.recode.vcf --missing-site
-
-echo "done30%, do 15%"
-
-vcftools --vcf populations.snps.vcf --max-missing 0.15 --maf 0.05 --minDP 3 --recode --recode-INFO-all --out miss15maf5dp3
-vcftools --vcf miss15maf5dp3.recode.vcf --missing-indv
-awk '$5 > 0.80' out.imiss | cut -f1 > lowDP-80.indv
-vcftools --vcf miss15maf5dp3.recode.vcf --remove lowDP-80.indv --recode --recode-INFO-all --out miss15maf5dp3INDV
-vcftools --vcf miss15maf5dp3INDV.recode.vcf --min-meanDP 5 --recode --recode-INFO-all --out final15
-vcftools --vcf final15.recode.vcf --missing-site
 echo "done"
