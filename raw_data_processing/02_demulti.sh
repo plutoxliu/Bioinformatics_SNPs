20f7250cb02bb9fbcf4aebe75b622f1e1121a016
plutoxliu <117046241+plutoxliu@users.noreply.github.com>
2026-03-13 23:31:02 +1300

    Enhance script with comments for process_radtags
    Added comments to clarify process_radtags options.

--- raw_data_processing/02_demulti.sh
+++ raw_data_processing/02_demulti.sh
@@ -11,6 +11,17 @@
 module purge
 module load Stacks/2.61-gimkl-2022a
 ####remember to create dictionary Demulti
+#-p: input sequence file
+#-o: output dictionary
+#-b: barcodes info
+#-e: restriction enzyme used
+#-r: rescue barcodes and RAD-Tag cut sites
+#-c: clean data, remove any read with an uncalled base
+#-q: discard reads with low quality scores
+#-P: files contained within the directory are paired
+#--threads: number of threads to run
+#--inline-inline: barcode is inline with sequence, occurs on single and paired-end read
+#--filter-illumina: discard reads that have been marked by Illumina’s chastity/purity filter as failing
 echo "load stacks, start run"
 
 process_radtags -p /nesi/project/uoo03773/NZFL2/Cut -o /nesi/project/uoo03773/NZFL2/Demulti -b /nesi/project/uoo03773/NZFL/NZFL_codes.txt -e pstI -r -c -q -P --threads 4 --inline-inline --filter-illumina
